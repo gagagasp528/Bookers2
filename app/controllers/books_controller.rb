21aac097669966793a663b91c2bb6670e7680f08
gagagasp528 <gagagasp528@gmail.com>
2021-07-15 03:06:52 +0000

    update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,14 +1,19 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.all
+    @book = Book.new
   end
 
   def show
+    @book = Book.find(params{:id})
   end
 
   def new
+    @book = Book.new
   end
 
   def edit
+    @book = Book.find(params{:id})
   end
 
   def create
@@ -19,3 +24,9 @@ class BooksController < ApplicationController
 
 
 end
+
+ private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image)
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   get 'books/edit'
   devise_for :users
    resources :books
-    resources :users, only: [:show, :edit, :update]
+    resources :users, only: [:index :show, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: 'homes#top'
 
