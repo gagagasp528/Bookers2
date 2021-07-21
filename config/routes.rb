81bababfae0b75eabb955d88798902743e651de2
gagagasp528 <gagagasp528@gmail.com>
2021-07-21 08:40:15 +0000

    c

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :books
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-get 'homes/about' => 'homes#about'
+get 'home/about' => 'homes#about'
 root to: 'homes#top'
 
 end
